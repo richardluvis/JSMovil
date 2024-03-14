11bb4ffeca857781cb30a5e08a6a179482220459
richardluvis <lopezcard35@gmail.com>
2024-03-13 21:20:39 -0500

    hecho finnnn

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 function getVideo(){
     var video = $("#search").val();
     
-    if(city != ''){
+    if(video != ''){
         
         $.ajax({
             // https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=java_script&key=AIzaSyB8IbXTJnV3n2ye-6CjXsfCVWIqq8ex1Fw
@@ -19,6 +19,7 @@ function getVideo(){
             dataType: "jsonp",
             success: function(data){
                 var widget = showResults(data)
+                
                 
                 
                 $("#showVideo").html(widget);
@@ -37,8 +38,23 @@ function getVideo(){
 }
 
 
-function showResults(data){
-    return  '<h2 style="font-weight:bold; font-size:30px; padding-top:20px;" class="text-center">Current Weather for '+data.name+', '+data.sys.country+'</h2>'+
+function showResults(data)
+{
+    console.log(data);
+    const videoTitle = data.items[0].snippet.title;
+    console.log(data.items.length);
+    var table = '';
+
+    for(var i = 0; i < data.items.length; i++){
+        table+='<div id="video" class="row">';
+        table+='<h2>'+data.items[i].snippet.title+'</h2>';
+        table+='<iframe width="640" height="360" src="https://www.youtube.com/embed/'+ data.items[i].id.videoId+'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+        table+='<p>Description: '+data.items[i].snippet.description+'</p>'
+        table+='</div>';
+    }  
+    $("#showVideo").html(table);
+    return 
+           /* '<h2 style="font-weight:bold; font-size:30px; padding-top:20px;" class="text-center">Current Weather for '+data.name+', '+data.sys.country+'</h2>'+
             "<h3 style='padding-left:40px;'><strong>Weather</strong>: "+data.weather[0].main+"</h3>"+
             "<h3 style='padding-left:40px;'><strong>Description</strong>:<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'> "+data.weather[0].description+"</h3>"+
             "<h3 style='padding-left:40px;'><strong>Temperature</strong>: "+data.main.temp+" &deg;C</h3>"+
@@ -48,5 +64,7 @@ function showResults(data){
             "<h3 style='padding-left:40px;'><strong>Max Temperature</strong>: "+data.main.temp_max+"&deg;C</h3>"+
             "<h3 style='padding-left:40px;'><strong>Wind Speed</strong>: "+data.wind.speed+"m/s</h3>"+
             "<h3 style='padding-left:40px; padding-bottom:30px;'><strong>Wind Direction</strong>: "+data.wind.deg+"&deg;</h3>";
+            */
+           ;
 }
 
